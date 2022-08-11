29616487bffc099b02f0f95eae081a006ad01394
Iaroslav Elistratov <iaroslav.elistratov@gmail.com>
2022-08-11 10:30:44 -0400

    unet with ensemble wrapper

--- experiments/train.py
+++ experiments/train.py
@@ -10,31 +10,82 @@ from keras.callbacks import CSVLogger
 
 from models import create
 from run_utils import setup
-from utils import load_depth_data, visualize_depth_map, plot_loss
+from utils import load_depth_data, load_apollo_data, visualize_depth_map, visualize_depth_map_uncertainty, plot_loss
 
 # tf logging - don't print INFO messages
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
 
+LR = 5e-5
+
+
+
 visualizations_path, checkpoints_path, plots_path, logs_path = setup()
+logger = CSVLogger(f'{logs_path}/log.csv', append=True)
 
 ### https://github.com/aamini/evidential-deep-learning/blob/main/neurips2020/train_depth.py#L34
 (x_train, y_train), (x_test, y_test) = load_depth_data()
 x_train = tf.convert_to_tensor(x_train[:256], tf.float32) #16, 256, 1024
 y_train = tf.convert_to_tensor(y_train[:256], tf.float32) #16, 256, 1024
-
 x_train /= 255.
 y_train /= 255.
+
+_, (x_test_ood, y_test_ood) = load_apollo_data()
+x_test_ood = tf.convert_to_tensor(x_test_ood[:256], tf.float32) #16, 256, 1024
+y_test_ood = tf.convert_to_tensor(y_test_ood[:256], tf.float32) #16, 256, 1024
+x_test_ood /= 255.
+y_test_ood /= 255.
 
 their_model = create(input_shape=x_train.shape[1:], drop_prob=0.0, activation=tf.nn.relu, num_class=1)
 # trainer = trainer_obj(model, opts, args.learning_rate)
 
-their_model.compile(
-    optimizer=keras.optimizers.Adam(learning_rate=5e-5),
-    loss=keras.losses.MeanSquaredError(),
+
+
+
+
+### train base model only (debugging)
+# their_model.compile(
+#     optimizer=keras.optimizers.Adam(learning_rate=LR),
+#     loss=keras.losses.MeanSquaredError(),
+# )
+
+# history = their_model.fit(x_train, y_train, epochs=256, batch_size=8, callbacks=[logger], verbose=0) # 10000 epochs
+# plot_loss(history, plots_path)
+
+# todo-high: inference on val set
+# pred = model(x_train, verbose=0)
+# visualize_depth_map(x_train, y_train, pred, visualizations_path)
+
+
+
+from capsa import (
+    Wrapper,
+    MVEWrapper,
+    EnsembleWrapper,
 )
 
-logger = CSVLogger(f'{logs_path}/log.csv', append=True)
-history = their_model.fit(x_train, y_train, epochs=256, batch_size=8, callbacks=[logger], verbose=0) # 10000 epochs
+model = EnsembleWrapper(their_model, num_members=2)
+model.compile(
+    optimizer=[keras.optimizers.Adam(learning_rate=LR)],
+    loss=[keras.losses.MeanSquaredError()],
+    # metrics=[[
+    #     # keras.metrics.MeanSquaredError(name='mse'),
+    #     keras.metrics.CosineSimilarity(name='cos'),
+    # ]],
+)
 
+history = model.fit(x_train, y_train, epochs=256, batch_size=8, callbacks=[logger], verbose=0) # 10000 epochs
 plot_loss(history, plots_path)
-visualize_depth_map(x_train, y_train, their_model, visualizations_path)
+
+def get_ensemble_uncertainty(x_train, model):
+    outs = model(x_train)
+    preds = np.concatenate((outs[0][np.newaxis, ...], outs[1][np.newaxis, ...]), 0)
+    pred = np.mean(preds, 0)
+    epistemic = np.std(preds, 0)
+
+    return pred, epistemic
+
+pred, epistemic = get_ensemble_uncertainty(x_train, model)
+visualize_depth_map_uncertainty(x_train, y_train, pred, epistemic, visualizations_path, 'iid.png')
+
+pred_ood, epistemic_ood = get_ensemble_uncertainty(x_test_ood, model)
+visualize_depth_map_uncertainty(x_test_ood, y_test_ood, pred_ood, epistemic_ood, visualizations_path, 'ood.png')
--- experiments/utils.py
+++ experiments/utils.py
@@ -7,20 +7,34 @@ def load_depth_data():
     test = h5py.File("/home/iaroslavelistratov/data/depth_test.h5", "r")
     return (train["image"], train["depth"]), (test["image"], test["depth"])
 
-# def load_apollo_data():
-#     test = h5py.File("/home/iaroslavelistratov/data/apolloscape_test.h5", "r")
-#     return (None, None), (test["image"], test["depth"])
+def load_apollo_data():
+    test = h5py.File("/home/iaroslavelistratov/data/apolloscape_test.h5", "r")
+    return (None, None), (test["image"], test["depth"])
 
-def visualize_depth_map(x, y, model, visualizations_path, name='map.png'):
+def visualize_depth_map(x, y, pred, visualizations_path, name='map.png'):
     cmap = plt.cm.jet
     cmap.set_bad(color="black")
 
-    pred = model.predict(x, verbose=0)
     fig, ax = plt.subplots(6, 3, figsize=(50, 50))
     for i in range(6):
         ax[i, 0].imshow(x[i])
         ax[i, 1].imshow(y[i, :, :, 0], cmap=cmap)
         ax[i, 2].imshow(pred[i, :, :, 0], cmap=cmap)
+
+    plt.savefig(f'{visualizations_path}/{name}')
+    plt.show()
+
+# hacky ugly way - should reuse visualize_depth_map
+def visualize_depth_map_uncertainty(x, y, pred, uncertain, visualizations_path, name='map.png'):
+    cmap = plt.cm.jet
+    cmap.set_bad(color="black")
+
+    fig, ax = plt.subplots(6, 4, figsize=(50, 50))
+    for i in range(6):
+        ax[i, 0].imshow(x[i])
+        ax[i, 1].imshow(y[i, :, :, 0], cmap=cmap)
+        ax[i, 2].imshow(pred[i, :, :, 0], cmap=cmap)
+        ax[i, 3].imshow(uncertain[i, :, :, 0], cmap=cmap)
 
     plt.savefig(f'{visualizations_path}/{name}')
     plt.show()
