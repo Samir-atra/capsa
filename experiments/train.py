b686df814676cf45f175b81dcca3b3a4fb28ff87
Iaroslav Elistratov <iaroslav.elistratov@gmail.com>
2022-08-13 13:11:29 -0400

    [refactor] move vis and model loading to utils.py

--- experiments/train.py
+++ experiments/train.py
@@ -1,66 +1,45 @@
-N_SAMPLES = 64 # 256
-BS = 32 # 8
-EP = 32 # 256
-LR = 5e-5 # 5e-5
-
-
 import os
 import numpy as np
-import matplotlib.pyplot as plt
 
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import layers
 from keras.callbacks import CSVLogger
-# import pandas as pd
 
+import config
 from capsa import Wrapper, MVEWrapper, EnsembleWrapper
 from models import create
 from run_utils import setup
-from utils import load_depth_data, load_apollo_data, visualize_depth_map, visualize_depth_map_uncertainty, plot_loss
+from utils import load_depth_data, load_apollo_data, totensor_and_normalize, \
+    visualize_depth_map, visualize_depth_map_uncertainty, plot_multiple, \
+    plot_loss
 
-# tf logging - don't print INFO messages
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '1'
-
-### https://github.com/aamini/evidential-deep-learning/blob/main/neurips2020/train_depth.py#L34
 (x_train, y_train), (x_test, y_test) = load_depth_data()
-x_train = tf.convert_to_tensor(x_train[:N_SAMPLES], tf.float32) #16, 256, 1024
-y_train = tf.convert_to_tensor(y_train[:N_SAMPLES], tf.float32) #16, 256, 1024
-x_train /= 255.
-y_train /= 255.
+x_train, y_train = totensor_and_normalize(x_train, y_train)
 
 _, (x_test_ood, y_test_ood) = load_apollo_data()
-x_test_ood = tf.convert_to_tensor(x_test_ood[:N_SAMPLES], tf.float32) #16, 256, 1024
-y_test_ood = tf.convert_to_tensor(y_test_ood[:N_SAMPLES], tf.float32) #16, 256, 1024
-x_test_ood /= 255.
-y_test_ood /= 255.
-
+x_test_ood, y_test_ood = totensor_and_normalize(x_test_ood, y_test_ood)
 
 def train_base_model():
-    visualizations_path, checkpoints_path, plots_path, logs_path = setup()
+    vis_path, checkpoints_path, plots_path, logs_path = setup()
     logger = CSVLogger(f'{logs_path}/log.csv', append=True)
 
     their_model = create(x_train.shape[1:])
-    # trainer = trainer_obj(model, opts, args.learning_rate)
-
     their_model.compile(
         optimizer=keras.optimizers.Adam(learning_rate=5e-5),
         loss=keras.losses.MeanSquaredError(),
     )
 
-    history = their_model.fit(x_train, y_train, epochs=256, batch_size=8, callbacks=[logger], verbose=0) # 10000 epochs
+    history = their_model.fit(x_train, y_train, epochs=256, batch_size=8, callbacks=[logger], verbose=0)
     plot_loss(history, plots_path)
 
-    # todo-high: inference on val set
     pred = their_model(x_train)
-    visualize_depth_map(x_train, y_train, pred, visualizations_path)
+    visualize_depth_map(x_train, y_train, pred, vis_path)
 
 def train_ensemble_wrapper():
-    visualizations_path, checkpoints_path, plots_path, logs_path = setup()
+    vis_path, checkpoints_path, plots_path, logs_path = setup()
     logger = CSVLogger(f'{logs_path}/log.csv', append=True)
 
     their_model = create(x_train.shape[1:])
-    # trainer = trainer_obj(model, opts, args.learning_rate)
 
     model = EnsembleWrapper(their_model, num_members=2)
     model.compile(
@@ -69,7 +48,7 @@ def train_ensemble_wrapper():
         # metrics=[[keras.metrics.CosineSimilarity(name='cos')]],
     )
 
-    history = model.fit(x_train, y_train, epochs=256, batch_size=8, callbacks=[logger], verbose=0) # 10000 epochs
+    history = model.fit(x_train, y_train, epochs=256, batch_size=8, callbacks=[logger], verbose=0)
     plot_loss(history, plots_path)
 
     def get_ensemble_uncertainty(x_train, model):
@@ -79,30 +58,25 @@ def train_ensemble_wrapper():
         return pred, epistemic
 
     pred, epistemic = get_ensemble_uncertainty(x_train, model)
-    visualize_depth_map_uncertainty(x_train, y_train, pred, epistemic, visualizations_path, 'iid.png')
+    visualize_depth_map_uncertainty(x_train, y_train, pred, epistemic, vis_path, 'iid.png')
 
     pred_ood, epistemic_ood = get_ensemble_uncertainty(x_test_ood, model)
-    visualize_depth_map_uncertainty(x_test_ood, y_test_ood, pred_ood, epistemic_ood, visualizations_path, 'ood.png')
+    visualize_depth_map_uncertainty(x_test_ood, y_test_ood, pred_ood, epistemic_ood, vis_path, 'ood.png')
 
 # train_base_model()
 # train_ensemble_wrapper()
 
 
 
-visualizations_path, checkpoints_path, plots_path, logs_path = setup()
+vis_path, checkpoints_path, plots_path, logs_path = setup()
 logger = CSVLogger(f'{logs_path}/log.csv', append=True)
 
 their_model = create(x_train.shape[1:])
-# trainer = trainer_obj(model, opts, args.learning_rate)
-
 model = MVEWrapper(their_model)
-model.compile(
-    optimizer=keras.optimizers.Adam(learning_rate=LR),
-    # loss=keras.losses.MeanSquaredError(),
-)
+model.compile(optimizer=keras.optimizers.Adam(learning_rate=config.LR))
 
-itters_per_ep = N_SAMPLES / BS
-total_itters = itters_per_ep * EP
+itters_per_ep = config.N_SAMPLES / config.BS
+total_itters = itters_per_ep * config.EP
 save_itters = int(total_itters // 10) # save 10 times during training
 # save_ep = int(save_itters / itters_per_ep)
 # last_saved_ep = round(save_itters * 10 // itters_per_ep)
@@ -121,45 +95,6 @@ model_checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     save_freq=save_itters, # batches, not epochs
 )
 
-history = model.fit(x_train, y_train, epochs=EP, batch_size=BS, callbacks=[logger, model_checkpoint_callback], verbose=0) # 10000 epochs
+history = model.fit(x_train, y_train, epochs=config.EP, batch_size=config.BS, callbacks=[logger, model_checkpoint_callback], verbose=0) # 10000 epochs
 plot_loss(history, plots_path)
-
-### need this to load weights
-del model
-their_model = create(x_train.shape[1:])
-model = MVEWrapper(their_model)
-model.compile(optimizer=keras.optimizers.Adam(learning_rate=LR))
-# https://github.com/tensorflow/tensorflow/issues/33150#issuecomment-574517363
-_ = model.fit(x_train, y_train, epochs=1, batch_size=BS, verbose=0)
-_, _ = model(x_train)
-latest = tf.train.latest_checkpoint(checkpoints_path)
-load_status = model.load_weights(latest)
-# used as validation that all variable values have been restored from the checkpoint
-load_status.assert_consumed()
-print(f'successfully loaded weights from {latest}')
-
-
-### plot
-# because if just to range(0, N_SAMPLES, BS) it's num_save_times=N_SAMPLES/BS which can be a huge number if dataset is big
-num_save_times = min(N_SAMPLES//BS, 10)
-
-for i in range(0, num_save_times*BS, BS):
-    # print(i, i+6)
-    x, y = x_train[i:i+6], y_train[i:i+6]
-    x_ood, y_ood = x_test_ood[i:i+6], y_test_ood[i:i+6]
-
-    pred, variance = model(x) # (6, 128, 160, 1) and (6, 128, 160, 1)
-    pred_ood, variance_ood = model(x_ood)
-
-    # normalize separately
-    # variance_normalized = (variance - np.min(variance)) / (np.max(variance) - np.min(variance))
-    # variance_ood_normalized = (variance_ood - np.min(variance_ood)) / (np.max(variance_ood) - np.min(variance_ood))
-
-    # normalize tougher
-    cat = tf.stack([variance, variance_ood]) #(6, 128, 160, 1), (6, 128, 160, 1) = (2, 6, 128, 160, 1)
-    cat_normalized = (cat - np.min(cat)) / (np.max(cat) - np.min(cat))
-    variance_normalized = cat_normalized[0]
-    variance_ood_normalized = cat_normalized[1]
-
-    visualize_depth_map_uncertainty(x, y, pred, variance_normalized, visualizations_path, f'{i}_iid.png')
-    visualize_depth_map_uncertainty(x_ood, y_ood, pred_ood, variance_ood_normalized, visualizations_path, f'{i}_ood.png')
+plot_multiple(model, x_train, y_train, x_test_ood, y_test_ood, vis_path)
--- experiments/utils.py
+++ experiments/utils.py
@@ -2,6 +2,14 @@ import h5py
 import numpy as np
 import matplotlib.pyplot as plt
 
+import tensorflow as tf
+from tensorflow import keras
+
+import config
+from capsa import Wrapper, MVEWrapper, EnsembleWrapper
+from models import create
+
+# https://github.com/aamini/evidential-deep-learning/blob/main/neurips2020/train_depth.py#L34
 def load_depth_data():
     train = h5py.File("/home/iaroslavelistratov/data/depth_train.h5", "r")
     test = h5py.File("/home/iaroslavelistratov/data/depth_test.h5", "r")
@@ -11,7 +19,21 @@ def load_apollo_data():
     test = h5py.File("/home/iaroslavelistratov/data/apolloscape_test.h5", "r")
     return (None, None), (test["image"], test["depth"])
 
-def visualize_depth_map(x, y, pred, visualizations_path, name='map.png'):
+def totensor_and_normalize(x, y):
+    x = tf.convert_to_tensor(x[:config.N_SAMPLES], tf.float32)
+    y = tf.convert_to_tensor(y[:config.N_SAMPLES], tf.float32)
+    return x / 255. , y / 255.
+
+def plot_loss(history, plots_path, name='loss'):
+    for k, v in history.history.items():
+        plt.plot(v, label=k)
+    plt.legend(loc='upper right')
+
+    plt.savefig(f'{plots_path}/{name}.pdf', bbox_inches='tight', format='pdf')
+    # plt.show()
+    plt.close()
+
+def visualize_depth_map(x, y, pred, visualizations_path, name='map'):
     cmap = plt.cm.jet
     cmap.set_bad(color="black")
 
@@ -21,12 +43,12 @@ def visualize_depth_map(x, y, pred, visualizations_path, name='map.png'):
         ax[i, 1].imshow(y[i, :, :, 0], cmap=cmap)
         ax[i, 2].imshow(pred[i, :, :, 0], cmap=cmap)
 
-    plt.savefig(f'{visualizations_path}/{name}')
+    plt.savefig(f'{visualizations_path}/{name}.pdf', bbox_inches='tight', format='pdf')
     # plt.show()
     plt.close()
 
 # hacky ugly way - should reuse visualize_depth_map
-def visualize_depth_map_uncertainty(x, y, pred, uncertain, visualizations_path, name='map.png'):
+def visualize_depth_map_uncertainty(x, y, pred, uncertain, visualizations_path, name='map'):
     cmap = plt.cm.jet
     cmap.set_bad(color="black")
 
@@ -37,15 +59,48 @@ def visualize_depth_map_uncertainty(x, y, pred, uncertain, visualizations_path, 
         ax[i, 2].imshow(pred[i, :, :, 0], cmap=cmap)
         ax[i, 3].imshow(uncertain[i, :, :, 0], cmap=cmap)
 
-    plt.savefig(f'{visualizations_path}/{name}')
+    plt.savefig(f'{visualizations_path}/{name}.pdf', bbox_inches='tight', format='pdf')
     # plt.show()
     plt.close()
 
-def plot_loss(history, plots_path, name='loss.png'):
-    for k, v in history.history.items():
-        plt.plot(v, label=k)
-    plt.legend(loc='upper right')
+def plot_multiple(model, x_train, y_train, x_test_ood, y_test_ood, vis_path, prefix=''):
+    # because if just to range(0, N_SAMPLES, BS) it's num_save_times=N_SAMPLES/BS which can be a huge number if dataset is big
+    num_save_times = min(config.N_SAMPLES//config.BS, config.NUM_PLOTS)
 
-    plt.savefig(f'{plots_path}/{name}')
-    # plt.show()
-    plt.close()
+    for i in range(0, num_save_times * config.BS, config.BS):
+        x, y = x_train[i:i+6], y_train[i:i+6]
+        x_ood, y_ood = x_test_ood[i:i+6], y_test_ood[i:i+6]
+
+        pred, variance = model(x) # (6, 128, 160, 1), (6, 128, 160, 1)
+        pred_ood, variance_ood = model(x_ood)
+
+        # normalize separately
+        # variance_normalized = (variance - np.min(variance)) / (np.max(variance) - np.min(variance))
+        # variance_ood_normalized = (variance_ood - np.min(variance_ood)) / (np.max(variance_ood) - np.min(variance_ood))
+
+        # normalize tougher
+        cat = tf.stack([variance, variance_ood]) #(6, 128, 160, 1), (6, 128, 160, 1) = (2, 6, 128, 160, 1)
+        cat_normalized = (cat - np.min(cat)) / (np.max(cat) - np.min(cat))
+        variance_normalized = cat_normalized[0]
+        variance_ood_normalized = cat_normalized[1]
+
+        visualize_depth_map_uncertainty(x, y, pred, variance_normalized, vis_path, f'{prefix}{i}_iid.png')
+        visualize_depth_map_uncertainty(x_ood, y_ood, pred_ood, variance_ood_normalized, vis_path, f'{prefix}{i}_ood.png')
+
+def load_model(path, x, y):
+    # path = tf.train.latest_checkpoint(checkpoints_path)
+
+    their_model = create(x.shape[1:])
+    # todo-med: make work with other wrappers
+    model = MVEWrapper(their_model)
+    model.compile(optimizer=keras.optimizers.Adam(learning_rate=config.LR))
+
+    # https://github.com/tensorflow/tensorflow/issues/33150#issuecomment-574517363
+    _ = model.fit(x, y, epochs=1, batch_size=config.BS, verbose=0)
+    _, _ = model(x[:config.BS])
+
+    load_status = model.load_weights(path)
+    # used as validation that all variable values have been restored from the checkpoint
+    load_status.assert_consumed()
+    print(f'Successfully loaded weights from {path}.')
+    return model
