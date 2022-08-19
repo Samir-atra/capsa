80ff0278671bd4350943e2c084f02b1e377285e1
Iaroslav Elistratov <iaroslav.elistratov@gmail.com>
2022-08-19 07:39:57 -0400

    loading from_config doesn't help

--- experiments/debug_minimal.py
+++ experiments/debug_minimal.py
@@ -4,29 +4,20 @@ from keras import optimizers as optim
 
 
 class DebugWrappar(keras.Model):
-    def __init__(
-        self, base_model, metric_wrapper=None, is_standalone=True
-    ):
+    def __init__(self, base_model, metric_wrapper=None, is_standalone=True):
         super(DebugWrappar, self).__init__()
 
-        self.metric_name = "ensemble"
+        self.metric_name = "debug"
         self.is_standalone = is_standalone
         self.base_model = base_model
 
         self.metric_wrapper = metric_wrapper
-        self.num_members = 1
         self.metrics_compiled = {}
 
     def compile(self, optimizer, loss):
         super(DebugWrappar, self).compile()
 
-        base_model_config = self.base_model.get_config()
-        assert base_model_config != {}, "Please implement get_config()."
-
-        # if isinstance(self.base_model, keras.Sequential):
-        #     m = keras.Sequential.from_config(base_model_config)
-        m = keras.Model.from_config(base_model_config)
-
+        m = self.base_model
         m = (m if self.metric_wrapper is None else self.metric_wrapper(m, self.is_standalone))
         m_name = (f"usermodel" if self.metric_wrapper is None else f"{m.metric_name}")
         m.compile(optimizer, loss)
