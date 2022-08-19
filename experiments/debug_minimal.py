a56b0b3e5dadfd3e7f153ed6ecf93d6309a41a44
Iaroslav Elistratov <iaroslav.elistratov@gmail.com>
2022-08-19 09:53:29 -0400

    reduce debug wrapper

--- experiments/debug_minimal.py
+++ experiments/debug_minimal.py
@@ -4,29 +4,18 @@ from keras import optimizers as optim
 
 
 class DebugWrappar(keras.Model):
-    def __init__(self, base_model, metric_wrapper=None, is_standalone=True):
+    def __init__(self, base_model):
         super(DebugWrappar, self).__init__()
 
         self.metric_name = "debug"
-        self.is_standalone = is_standalone
-        self.metric_wrapper = metric_wrapper
-
-        self.model_name = (f"usermodel" if self.metric_wrapper is None else f"{m.metric_name}")
         self.base_model = base_model
 
     def train_step(self, data):
         keras_metrics = {}
 
-        # user model
-        if self.metric_wrapper is None:
-            _ = self.base_model.train_step(data)
-            for m in self.base_model.metrics:
-                keras_metrics[f"{self.model_name}_{m.name}"] = m.result()
-
-        # one of our metrics
-        else:
-            keras_metric = self.base_model.train_step(data, self.model_name)
-            keras_metrics.update(keras_metric)
+        _ = self.base_model.train_step(data)
+        for m in self.base_model.metrics:
+            keras_metrics[f"{self.metric_name}_{m.name}"] = m.result()
 
         return keras_metrics
 
@@ -34,29 +23,16 @@ class DebugWrappar(keras.Model):
     def test_step(self, data):
         keras_metrics = {}
 
-        # user model
-        if self.metric_wrapper is None:
-            _ = self.base_model.test_step(data)
-            for m in self.base_model.metrics:
-                keras_metrics[f"{self.model_name}_{m.name}"] = m.result()
-
-        # one of our metrics
-        else:
-            keras_metric = self.base_model.test_step(data, self.model_name)
-            keras_metrics.update(keras_metric)
+        _ = self.base_model.test_step(data)
+        for m in self.base_model.metrics:
+            keras_metrics[f"{self.metric_name}_{m.name}"] = m.result()
 
         return keras_metrics
 
     def call(self, x, training=False, return_risk=True, features=None):
         outs = []
 
-        # ensembling the user model
-        if self.metric_wrapper is None:
-            out = self.base_model(x)
-
-        # ensembling one of our own metrics
-        else:
-            out = self.base_model(x, training, return_risk, features)
+        out = self.base_model(x)
         outs.append(out)
 
         preds = tf.stack(outs)
