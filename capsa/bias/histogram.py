a25ede0e9ba20b0094b75751192c498433d2d170
Sadhana Lolla <lolla.sadhana@gmail.com>
2022-08-29 17:05:11 -0400

    more updates

--- capsa/bias/histogram.py
+++ capsa/bias/histogram.py
@@ -4,7 +4,7 @@ from tensorflow import keras
 from ..wrapper import Wrapper
 from ..utils import copy_layer
 import tensorflow_probability as tfp
-
+import numpy as np
 
 
 class HistogramWrapper(keras.Model):
@@ -13,12 +13,10 @@ class HistogramWrapper(keras.Model):
 
         Args:
             base_model (model): the model to generate features from
-            metric_wrapper: currently can only be a VAE and the 
-                histogram will be constructed with these features instead if not None.
             num_bins: how many bins to use in the histogram
     """
 
-    def __init__(self, base_model, is_standalone=True, num_bins=5, metric_wrapper=None):
+    def __init__(self, base_model, is_standalone=True, num_bins=5):
         super(HistogramWrapper, self).__init__()
         self.base_model = base_model
         self.metric_name = "HistogramWrapper"
@@ -33,30 +31,10 @@ class HistogramWrapper(keras.Model):
 
         self.histogram_layer = HistogramLayer(num_bins=num_bins)
 
-        # currently only supports VAEs!
-        self.metric_wrapper = metric_wrapper
-
-    def compile(self, optimizer, loss, *args, **kwargs):
-        # replace the given feature extractor with the metric wrapper's extractor if provided
-        if self.metric_wrapper is not None:
-            if type(self.metric_wrapper) == type:
-                # we have received an uninitialized wrapper
-                self.metric_wrapper = self.metric_wrapper(
-                    base_model=self.base_model, is_standalone=self.is_standalone,
-                )
-                self.metric_wrapper.compile(
-                    optimizer=optimizer, loss=loss, *args, **kwargs
-                )
-            self.output_layer = self.metric_wrapper.output_layer
-            self.feature_extractor = self.metric_wrapper.feature_extractor
-
-        super(HistogramWrapper, self).compile(optimizer=optimizer, loss=loss, **kwargs)
-
     def loss_fn(self, x, y, extractor_out=None):
         if extractor_out is None:
             extractor_out = self.feature_extractor(x, training=True)
-        hist_input = extractor_out
-        self.histogram_layer(hist_input)
+        self.histogram_layer(extractor_out, training=True)
         out = self.output_layer(extractor_out)
         loss = tf.reduce_mean(
             self.compiled_loss(y, out, regularization_losses=self.losses),
@@ -67,31 +45,18 @@ class HistogramWrapper(keras.Model):
     def train_step(self, data):
         x, y = data
         with tf.GradientTape() as t:
-            if self.metric_wrapper is not None:
-                _ = self.metric_wrapper.train_step(data)
-                loss, predictor_y = self.loss_fn(
-                    x, y, extractor_out=self.metric_wrapper.input_to_histogram(x)
-                )
-            else:
-                loss, predictor_y = self.loss_fn(x, y)
-        trainable_vars = self.trainable_variables
-        gradients = t.gradient(loss, trainable_vars)
-        self.optimizer.apply_gradients(zip(gradients, trainable_vars))
-        self.compiled_metrics.update_state(y, predictor_y)
-        return {m.name: m.result() for m in self.metrics}
+            loss, predictor_y = self.loss_fn(x, y)
+            trainable_vars = self.trainable_variables
+            gradients = t.gradient(loss, trainable_vars)
+            self.optimizer.apply_gradients(zip(gradients, trainable_vars))
+            self.compiled_metrics.update_state(y, predictor_y)
+            return {m.name: m.result() for m in self.metrics}
 
     @tf.function
     def wrapped_train_step(self, x, y, features, prefix):
 
         with tf.GradientTape() as t:
-            if self.metric_wrapper is not None:
-                loss, y_hat = self.loss_fn(
-                    x,
-                    y,
-                    self.metric_wrapper.input_to_histogram(x, extractor_out=features),
-                )
-            else:
-                loss, y_hat = self.loss_fn(x, y, features)
+            loss, y_hat = self.loss_fn(x, y, features)
         self.compiled_metrics.update_state(y, y_hat)
 
         trainable_vars = self.trainable_variables
@@ -103,16 +68,12 @@ class HistogramWrapper(keras.Model):
             tf.gradients(loss, features),
         )
 
-    def call(self, x, training=False, return_risk=True, features=None):
-        if self.is_standalone and self.metric_wrapper is None:
+    def call(self, x, training=False, return_risk=True, features=None, softmax=False):
+        if self.is_standalone:
             features = self.feature_extractor(x, training=False)
 
-        if self.metric_wrapper is not None:
-            # get the correct inputs to histogram if we have an additional metric
-            features = self.metric_wrapper.input_to_histogram(x, training=False)
-
         predictor_y = self.output_layer(features)
-        bias = self.histogram_layer(features, training=False)
+        bias = self.histogram_layer(features, training=training, softmax=softmax)
 
         return predictor_y, bias
 
@@ -145,7 +106,7 @@ class HistogramLayer(tf.keras.layers.Layer):
             initial_value=tf.zeros(input_shape[1:]), trainable=False
         )
 
-    def call(self, inputs, training=True):
+    def call(self, inputs, training=True, softmax=False):
         # Updates frequencies if we are training
         if training:
             self.minimums.assign(
@@ -165,7 +126,6 @@ class HistogramLayer(tf.keras.layers.Layer):
             self.num_batches.assign_add(1)
         else:
             # Returns the probability of a datapoint occurring if we are in inference mode
-
             # Normalize histograms
             hist_probs = tf.divide(
                 self.frequencies, tf.reduce_sum(self.frequencies, axis=0)
@@ -180,6 +140,7 @@ class HistogramLayer(tf.keras.layers.Layer):
                 ),
                 tf.dtypes.int32,
             )
+
             # Multiply probabilities together to compute bias
             second_element = tf.repeat(
                 [tf.range(tf.shape(inputs)[1])], repeats=[tf.shape(inputs)[0]], axis=0
