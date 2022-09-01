3afdf06db5be1acaae00e4145e9e983330a52b44
Iaroslav Elistratov <iaroslav.elistratov@gmail.com>
2022-09-01 17:53:29 -0400

    minor fix to calibration plots

--- experiments/visualizations.py
+++ experiments/visualizations.py
@@ -1,54 +1,43 @@
 import numpy as np
-
-import matplotlib.pyplot as plt
-
 import tensorflow as tf
-
-from capsa import Wrapper, HistogramWrapper, VAEWrapper, EnsembleWrapper, DropoutWrapper, MVEWrapper
-from capsa.utils import get_user_model, plt_vspan, plot_results, plot_loss
 from scipy import stats
 from tqdm import tqdm
+import matplotlib.pyplot as plt
 
-from utils import load_depth_data, load_apollo_data, get_normalized_ds, \
-    visualize_depth_map, plot_loss, load_model, totensor_and_normalize
-from models import unet
-import config
-from callbacks import CalibrationCallback
+from capsa import MVEWrapper
+from models import AutoEncoder
 
+def gen_calibration_plot(model, ds, path=None):
+    mu_ = []
+    std_ = []
+    y_test_ = []
 
-def gen_calibration_plot(model, path, ds_test):
-        percentiles = np.arange(41)/40
-        vals = []
-        mu = []
-        std = []
-        y_test = []
-        for step, (x_test_batch, y_test_batch) in enumerate(ds_test):
-            #outs = np.array([self.model(x_test_batch, training=True) for _ in range(20)])
-            #mu_batch = np.array([i[0] for i in outs])
-            #std_batch = np.array([i[1] for i in outs])
-            #total_mu = tf.math.reduce_mean(mu_batch, axis=0)
-            #mu.append(total_mu)
-            #std.append(tf.math.reduce_mean(std_batch + mu_batch, axis=0) - total_mu**2)
-            mu_batch, std_batch = model(x_test_batch)
-            mu.append(mu_batch)
-            std.append(std_batch)
-            y_test.append(y_test_batch)
-        mu = np.array(mu)
-        std = np.array(std)
-        print(tf.math.reduce_mean(std))
-        y_test = np.array(y_test)
-        y_test = y_test.reshape(-1, *y_test.shape[-3:])
-        mu = mu.reshape(-1, *mu.shape[-3:])
-        std = tf.sqrt(std.reshape(-1, *std.shape[-3:]))
-        
-        for percentile in tqdm(percentiles):
-            ppf_for_this_percentile = stats.norm.ppf(percentile, mu, std)
-            vals.append((y_test <= ppf_for_this_percentile).mean())
+    # x_test_batch, y_test_batch = iter(ds).get_next()
+    for (x_test_batch, y_test_batch) in ds:
+        # todo-med: better use model.predict, it's more optimized
+        mu_batch, std_batch = model(x_test_batch)
+        mu_.append(mu_batch)
+        std_.append(std_batch)
+        y_test_.append(y_test_batch)
 
-        plt.clf()
-        plt.plot(percentiles, vals)
-        plt.plot(percentiles, percentiles)
-        plt.title(str(np.mean(abs(percentiles - vals))))
-        plt.show()
+    mu = np.concatenate(mu_) #(3029, 128, 160, 1)
+    std = np.concatenate(std_) #(3029, 128, 160, 1)
+    y_test = np.concatenate(y_test_) #(3029, 128, 160, 1)
+
+    if isinstance(model, MVEWrapper):
+        std = np.sqrt(std)
+
+    vals = []
+    percentiles = np.arange(41)/40
+    for percentile in percentiles:
+        # returns the value at the n% percentile e.g., stats.norm.ppf(0.5, 0, 1) == 0.0
+        # in other words, if have a normal distrib. with mean 0 and std 1, 50% of data falls below and 50% falls above 0.0.
+        ppf_for_this_percentile = stats.norm.ppf(percentile, mu, std) # (3029, 128, 160, 1)
+        vals.append((y_test <= ppf_for_this_percentile).mean()) # (3029, 128, 160, 1) -> scalar
+
+    plt.plot(percentiles, vals)
+    plt.plot(percentiles, percentiles)
+    plt.title(str(np.mean(abs(percentiles - vals))))
+    plt.show()
+    if path != None:
         plt.savefig(path)
-        
