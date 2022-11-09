b7f10c8a0e799fe50da6440fad19216b5787c902
Elaheh Ahmadi <elahea@github.mit.edu>
2022-11-09 14:28:51 -0500

    fixed data path

--- projects/bb_data.py
+++ projects/bb_data.py
@@ -3,7 +3,7 @@ import pandas as pd
 from PIL import Image
 import numpy as np
 
-data_path="./test_data" #'/data/biasbounty/split'
+data_path = '/data/biasbounty/split'
 
 def load_split_data():
     label_map = {"gender": {"male": 1, "female": 2}, 
